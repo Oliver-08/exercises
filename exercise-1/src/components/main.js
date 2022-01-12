2e63245d6db8bbaacc371504fc16bca7eceafa66
unknown <you@example.com>
2022-01-12 19:22:41 +0100

    exercise

---
+++ exercise-1/src/components/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const SiteInfo = () => {
+    return (
+
+        <div className='site-info'>
+            <div className='title'>
+                HOSIA
+            </div>
+            <div className='sub-title'>
+                Web Design | Mobile Apps
+            </div>
+        </div>
+
+    );
+
+}
+
+export default SiteInfo;
